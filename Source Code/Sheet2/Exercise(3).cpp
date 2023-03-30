8ab91d4b8dc1139db585999e864cdbcea5668414
Shehab <127568346+dizzydroid@users.noreply.github.com>
2023-03-30 13:13:17 +0200

    Update Exercise(3).cpp

--- Source Code/Sheet2/Exercise(3).cpp
+++ Source Code/Sheet2/Exercise(3).cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 int main() {
 	float num1, num2, num3;
+	cout << "Please enter three numbers: ";
 	cin >> num1 >> num2 >> num3;
+	cout << "The largest number is: ";
 	cout << max({ num1,num2,num3 }) << endl;
 	return 0;
 }
