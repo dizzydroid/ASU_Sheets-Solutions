c4e2fa88822c6cbe4b0e30d3e4e435997b8ca2fe
dizzydroid <shehabmahmoud2003@gmail.com>
2024-02-25 01:08:46 +0200

    added sheet2 + soln

---
+++ [CSE231s]  Advanced Computer Prog. (JAVA)/[CSE231s] Source Code/Sheet2/ArrayMagnitude.java
@@ -0,0 +1,20 @@
+public class ArrayMagnitude extends Vector{
+    // average magnitude method
+    public static double averageMagnitude(Vector[] vectors){
+        double sum = 0;
+        for (Vector vector : vectors){
+            sum += vector.magnitude();
+        }
+        return sum / vectors.length;
+}
+
+    // main method
+    public static void main(String[] args){
+        // test cases
+        Vector[] vectors = new Vector[3];
+        vectors[0] = new Vector(3,4);
+        vectors[1] = new Vector(4,5);
+        vectors[2] = new Vector(5,6);
+        System.out.println("Average magnitude: " + averageMagnitude(vectors));
+    }
+}
